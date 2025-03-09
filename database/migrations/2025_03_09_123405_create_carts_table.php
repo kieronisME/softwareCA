6a00025ad75f07296d20805d46d1a6ba74e3511f
Kieron Tekumu <N00233918@iadt.ie>
2025-03-09 13:15:03 +0000

    finnshed thee cart migrations table now i will be adding the rest of the tables

--- database/migrations/2025_03_09_123405_create_carts_table.php
+++ database/migrations/2025_03_09_123405_create_carts_table.php
@@ -13,6 +13,57 @@ return new class extends Migration
     {
         Schema::create('carts', function (Blueprint $table) {
             $table->id();
+            //fk to users table
+            $table->foreignId('user_id')->constrained('users', 'user_id'); //1 
+
+            // fk to certfiedProduct  products
+            $table->foreignId('certfiedProduct_id')->constrained('certfiedProducts', 'certfiedProduct_id'); // 0
+
+            // fk to notCertifried products
+            $table->foreignId('notCertfiedProduct_id')->constrained('notCertfiedProducts', 'notCertfiedProduct_id'); // 0
+
+
+
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            //////////////////                                      ↓ NOT CERTFIED PRODUCTS ↓                                  ////////////////////
+            //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+            // fk to not certfied WOOD products table
+            $table->foreignId('notCertfiedWoodProducts_id')->constrained('notCertfiedWoodProducts', 'ncWoodProducts_id'); // 0
+
+            // fk to not certfied METAL products table
+            $table->foreignId('notCertfiedMetalProducts_id')->constrained('notCertfiedMetalProducts', 'notCertfiedMetalProducts_id'); // 0
+
+            // fk to not certfied STEEL products table
+            $table->foreignId('notCertfiedSteelProducts_id')->constrained('notCertfiedSteelProducts', 'notCertfiedSteelProducts_id'); // 0
+
+            // fk to not certfied ASPAHLT products table
+            $table->foreignId('notCertfiedAsphaltProducts_id')->constrained('notCertfiedAsphaltProducts', 'notCertfiedAsphaltProducts_id'); // 0
+
+
+
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            //////////////////                                      ↓ CERTFIED PRODUCTS ↓                                      ////////////////////
+            //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+            // fk to not certfied WOOD products table
+            $table->foreignId('CertfiedWoodProducts_id')->constrained('CertfiedWoodProducts', 'ncWoodProducts_id'); // 0
+
+            // fk to  certfied METAL products table
+            $table->foreignId('CertfiedMetalProducts_id')->constrained('CertfiedMetalProducts', 'CertfiedMetalProducts_id'); // 0
+
+            // fk to  certfied STEEL products table
+            $table->foreignId('CertfiedSteelProducts_id')->constrained('CertfiedSteelProducts', 'CertfiedSteelProducts_id'); // 0
+
+            // fk to  certfied ASPAHLT products table
+            $table->foreignId('CertfiedAsphaltProducts_id')->constrained('CertfiedAsphaltProducts', 'CertfiedAsphaltProducts_id'); // 0
+
+            // fk to Certifried products
+            $table->foreignId('user_id')->constrained('users', 'user_id'); // 0
+
+
+
             $table->timestamps();
         });
     }

3c69ce3ced11e993639612b57c3b69c3ee8b3aac
Kieron Tekumu <N00233918@iadt.ie>
2025-03-09 13:23:51 +0000

    added all NOT CERTFIED Products to the mrigrations table. ALL TABLES ADDED. onto populating them with fk's and emails and whatnot

---
+++ app/Http/Controllers/NotCertfiedAsphaltProductsController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\notCertfiedAsphaltProducts;
+use Illuminate\Http\Request;
+
+class NotCertfiedAsphaltProductsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(notCertfiedAsphaltProducts $notCertfiedAsphaltProducts)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(notCertfiedAsphaltProducts $notCertfiedAsphaltProducts)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, notCertfiedAsphaltProducts $notCertfiedAsphaltProducts)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(notCertfiedAsphaltProducts $notCertfiedAsphaltProducts)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/NotCertfiedWoodProductsController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\notCertfiedWoodProducts;
+use Illuminate\Http\Request;
+
+class NotCertfiedWoodProductsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(notCertfiedWoodProducts $notCertfiedWoodProducts)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(notCertfiedWoodProducts $notCertfiedWoodProducts)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, notCertfiedWoodProducts $notCertfiedWoodProducts)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(notCertfiedWoodProducts $notCertfiedWoodProducts)
+    {
+        //
+    }
+}

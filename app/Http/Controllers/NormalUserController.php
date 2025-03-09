6f0190e9cc08265b59ea225802df678178e0cfd5
Kieron Tekumu <N00233918@iadt.ie>
2025-03-09 12:58:01 +0000

    in the middle of adding all the tables needed for DB

---
+++ app/Http/Controllers/NormalUserController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\NormalUser;
+use Illuminate\Http\Request;
+
+class NormalUserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(NormalUser $normalUser)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(NormalUser $normalUser)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, NormalUser $normalUser)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(NormalUser $normalUser)
+    {
+        //
+    }
+}

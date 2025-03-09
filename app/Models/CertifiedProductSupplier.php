d4723ccbc94d62b3a31f692800cad48862dde13e
Kieron Tekumu <N00233918@iadt.ie>
2025-03-09 14:59:09 +0000

    added a one to many relation between supplier [one] and certfiedProductsSupliers [many] - my plan here is that i add all the migrations, fill them will place holder data until i find the real data, then add the relationships between tables and finnaly migrate them tyo the db. i undertsand that just figuring out an order to miragte them incarmentaly would be more efficaint esspecsaily when it comes to fixing anything but im stupid and wont do that

--- app/Models/CertifiedProductSupplier.php
+++ app/Models/CertifiedProductSupplier.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class CertifiedProductSupplier extends Model
 {
-    //
+    use HasFactory;
+
+
+
+
+
+    // to SUPPLIER ↓
+    //supplier is the ONE in the one to many relation ship supplier and Certfied Product Suplier have 
+    // a supplier can supply many products but a product can only belong to one supplier 
+    public function supplier()
+    {
+        return $this->belongsTo(Supplier::class);
+    }
 }
--- app/Models/Supplier.php
+++ app/Models/Supplier.php
@@ -2,9 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Supplier extends Model
 {
-    //
+    use HasFactory;
+
+
+
+    // to certfied products supplier table ↓
+    //certifiedProductsSuppliers is the MANY in the one to many relation ship supplier and Certfied Product Suplier have 
+    // a supplier can supply many products but a product can only belong to one supplier 
+    public function certifiedProductsSuppliers()
+    {
+        return $this->hasMany(CertifiedProductSupplier::class);
+    }
 }

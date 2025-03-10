0c44dd5f48d818a98a9b2da6783a0f4d5effa176
Kieron Tekumu <N00233918@iadt.ie>
2025-03-10 11:50:57 +0000

    SUCCESS: added cart_id to suppliers table

---
+++ database/migrations/2025_03_10_114907_add_cart_id_to_suppliers_table.php
@@ -0,0 +1,33 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('suppliers', function (Blueprint $table) {
+            // Add the foreign key 
+            $table->unsignedBigInteger('cart_id')->after('id'); 
+            $table->foreign('cart_id')
+                ->references('id')
+                ->on('carts')
+                ->onDelete('cascade'); 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('suppliers', function (Blueprint $table) {
+            $table->dropForeign(['cart_id']);
+            $table->dropColumn('cart_id');
+        });
+    }
+};

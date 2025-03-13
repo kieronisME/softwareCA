f16822d312a16133235fff5d8417f4cd4c13ebcd
Kieron Tekumu <N00233918@iadt.ie>
2025-03-13 21:55:57 +0000

    MAJOR DB ISSUE: the way in wich i have set up my many to many's is to complicated and might actually just not work at all i think. i have a table that holds my fks to another table for the many to many and i have the laravel naming scheme tables that it makes for many to many. i was going to see if i could blend them or something and it was just confusing me so i decied to renthink how the many to many is going to work: DB CHANGE: im going to try and link all the tables i stored in certified product and not certfied product to the cart table, each having a many to many to cart. ive started this process i have added the migrations nessessary for thsi change all i need to do is fill them with the right data and see how it looks

--- database/migrations/2025_03_13_213441_create_cart_certified_wood_table.php
+++ database/migrations/2025_03_13_213441_create_cart_certified_wood_table.php
@@ -11,14 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-
-// Schema::disableForeignKeyConstraints();
-        Schema::create('cart_not_certified_products', function (Blueprint $table) {
+        Schema::create('cart_certified_wood', function (Blueprint $table) {
             $table->id();
-
-            // $table->foreignId('cart_id')->constrained('carts'); 
-            // $table->foreignId('not_certfied_products_id')->constrained('not_certfied_products');
-            
             $table->timestamps();
         });
     }
@@ -28,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('cart_not_certified_products');
+        Schema::dropIfExists('cart_certified_wood');
     }
 };

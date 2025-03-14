b8d1734bb0bf28543a2f0d6f8e836ff0aacf6e3f
Kieron Tekumu <N00233918@iadt.ie>
2025-03-14 00:25:42 +0000

    db sucsefully changed into a cleaner neater less complicated version of itself. now every product table has its own many to many

--- database/migrations/2025_03_10_144455_add_fks_to_cart.php
+++ database/migrations/2025_03_10_144455_add_fks_to_cart.php
@@ -19,96 +19,6 @@ return new class extends Migration
                 ->references('id')
                 ->on('users')
                 ->onDelete('cascade');
-
-
-            //im adding the fk to my pivoit tables here because i want to enforce a many to many relation between 
-            // cart -> Certfied wood products table
-            // cart -> Certfied metal products table
-            // cart -> Certfied steel products table
-            // cart -> Certfied asphalt products table
-            //through the pivoit table notCertfiedProducts.
-            // im doing the same with my non certfied poroducts through the pivoit table notCertfiedProducts
-
-
-            ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-            //////////////////                                      ↓ Pivoit Tables ↓                                          ////////////////////
-            //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-            // fk to certfiedProduct pivoit table 
-            // $table->unsignedBigInteger('certfied_product_id');
-            // $table->foreign('certfied_product_id')
-            //     ->references('id')
-            //     ->on('certfied_products')
-            //     ->onDelete('cascade');
-
-
-            // // fk to notCertifried pivoit table
-            // $table->unsignedBigInteger('not_certfied_product_id');
-            // $table->foreign('not_certfied_product_id')
-            //     ->references('id')
-            //     ->on('not_certfied_products')
-            //     ->onDelete('cascade');
-
-
-
-            ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-            //////////////////                                      ↓ CERTFIED PRODUCTS ↓                                      ////////////////////
-            //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-            // fk to not certfied WOOD products table
-            $table->unsignedBigInteger('certfied_wood_products_id');
-            $table->foreign('certfied_wood_products_id')
-                ->references('id')
-                ->on('certfied_wood_products')
-                ->onDelete('cascade');
-
-            // fk to certfied METAL products table
-            $table->unsignedBigInteger('certfied_metal_products_id');
-            $table->foreign('certfied_metal_products_id')
-                ->references('id')
-                ->on('certfied_metal_products')
-                ->onDelete('cascade');
-
-
-            // fk to certfied STEEL products table
-            $table->unsignedBigInteger('certfied_steel_products_id');
-            $table->foreign('certfied_steel_products_id')
-                ->references('id')
-                ->on('certfied_steel_products')
-                ->onDelete('cascade');
-
-
-
-
-            ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-            //////////////////                                      ↓ NOT CERTFIED PRODUCTS ↓                                  ////////////////////
-            //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-            // fk to not certfied WOOD products table
-            $table->unsignedBigInteger('not_certfied_wood_products_id');
-            $table->foreign('not_certfied_wood_products_id')
-                ->references('id')
-                ->on('not_certfied_wood_products')
-                ->onDelete('cascade');
-
-            // fk to not certfied METAL products table
-            $table->unsignedBigInteger('not_certfied_metal_products_id');
-            $table->foreign('not_certfied_metal_products_id')
-                ->references('id')
-                ->on('not_certfied_metal_products')
-                ->onDelete('cascade');
-
-            // fk to not certfied STEEL products table
-            $table->unsignedBigInteger('not_certfied_steel_products_id');
-            $table->foreign('not_certfied_steel_products_id')
-                ->references('id')
-                ->on('not_certfied_steel_products')
-                ->onDelete('cascade');
-
-
-            // Timestamps (optional)
-            $table->timestamps();
         });
     }
 
@@ -118,30 +28,8 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('carts', function (Blueprint $table) {
-
             $table->dropForeign(['user_id']);
-            // $table->dropForeign(['certfied_product_id']);
-            // $table->dropForeign(['not_certfied_product_id']);
-            $table->dropForeign(['certfied_wood_products_id']);
-            $table->dropForeign(['certfied_metal_products_id']);
-            $table->dropForeign(['certfied_steel_products_id']);
-            $table->dropForeign(['not_certfied_wood_products_id']);
-            $table->dropForeign(['not_certfied_metal_products_id']);
-            $table->dropForeign(['not_certfied_steel_products_id']);
-
-
-
-            $table->dropColumn([
-                'user_id',
-                // 'certfied_product_id',
-                // 'not_certfied_product_id',
-                'certfied_wood_products_id',
-                'certfied_metal_products_id',
-                'certfied_steel_products_id',
-                'not_certfied_wood_products_id',
-                'not_certfied_metal_products_id',
-                'not_certfied_steel_products_id',
-            ]);
+            $table->dropColumn('user_id');
         });
     }
 };
